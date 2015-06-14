948342a70430e595f84caaaab2fe95eba56bfc4c
vialley-imac <jovilee09@gmail.com>
2015-06-14 13:54:05 +0800

    add tag101 for UILabel

--- MRT/ViewController.swift
+++ MRT/ViewController.swift
@@ -25,42 +25,55 @@ class ViewController: UIViewController, UITableViewDataSource {
     // 先印出所有中文站名即可
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - private
+    func routineSyncDepartureTime() {
+        DepartureManager.sharedInstance.syncMRTDepartureTime(completionBlock: { () -> Void in
+            println("==========")
+            for var i = 0; i < DepartureManager.sharedInstance.stations.count; ++i {
+                var st = DepartureManager.sharedInstance.stations[i]
+                println("\(st.cname) \(st.code)")
+                
+                for p:Platform in st.redLine {
+                    println("\(p.destination) \(p.arrivalTime) \(p.nextArrivalTime)")
+                }
+                
+                for p:Platform in st.orangeLine {
+                    println("\(p.destination) \(p.arrivalTime) \(p.nextArrivalTime)")
+                }
+                println("==========")
+            }
+        }, errorBlock: nil)
+    }
+    
     
     // MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20 //呈現的行數
+        //return DepartureManager.sharedInstance.stations.count
+        return 2  //呈現的行數.項
     }
 
     // 這行函數呈現的是，控制來源數據於每行呈現的樣子及效果
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("station") as! UITableViewCell
         
+        var station = DepartureManager.sharedInstance.stations[0]
+        var platform = station.redLine[0]
+        var time = platform.arrivalTime
+        
+        var stationTitle = cell.viewWithTag(101) as! UILabel
+        
+        stationTitle.text = station.DepartureManager.sharedInstance.stations[0].text
+        
+        
         return cell
     }
     
 
-  // MARK: - private
-  func routineSyncDepartureTime() {
-    DepartureManager.sharedInstance.syncMRTDepartureTime(completionBlock: { () -> Void in
-      println("==========")
-      for var i = 0; i < DepartureManager.sharedInstance.stations.count; ++i {
-        var st = DepartureManager.sharedInstance.stations[i]
-        println("\(st.cname) \(st.code)")
-        
-        for p:Platform in st.redLine {
-          println("\(p.destination) \(p.arrivalTime) \(p.nextArrivalTime)")
-        }
-        
-        for p:Platform in st.orangeLine {
-          println("\(p.destination) \(p.arrivalTime) \(p.nextArrivalTime)")
-        }
-        println("==========")
-      }
-    }, errorBlock: nil)
-  }
+
 }
 
